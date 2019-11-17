6caaa6e0ad643cbe99efb5d2fe40f0e883c7d36e
antran1111 <54298061+antran1111@users.noreply.github.com>
2019-11-16 23:03:24 -0800

    centered the widget

--- lib/main.dart
+++ lib/main.dart
@@ -18,22 +18,24 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Expanded(
-          //expanded widget spreads out to the entire screen
-          // flex: 2, //#2  = 2x as large as the other expanded widget
-          //if you don't set flex, they will take up equal space, default ==1
-          child: Image(
-            image: AssetImage('images/dice1.png'),
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            //expanded widget spreads out to the entire screen
+            // flex: 2, //#2  = 2x as large as the other expanded widget
+            //if you don't set flex, they will take up equal space, default ==1
+            child: Image(
+              image: AssetImage('images/dice1.png'),
+            ),
           ),
-        ),
-        Expanded(
-          //flex: 1,
-          child: Image.asset(
-              'images/dice2.png'), //this is another way to create images
-        ),
-      ],
+          Expanded(
+            //flex: 1,
+            child: Image.asset(
+                'images/dice1.png'), //this is another way to create images
+          ),
+        ],
+      ),
     );
     //return Container();
   }
